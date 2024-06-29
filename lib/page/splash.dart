753e9bfa5183c09679f9711716ab2d48ff37df32
ardan43 <ardan43@guru.smk.belajar.id>
2024-06-29 00:53:57 +0000

    pembuatan tampilan splash view

--- lib/page/splash.dart
+++ lib/page/splash.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
@@ -8,7 +7,16 @@ class SplashView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset('assets/images/logo.png'),
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:1188471074.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png'),
+            const SizedBox(height: 20),
+            const Text('Bluetooth Tomato Counter', style: TextStyle(fontSize: 24)),
+            const Text('v1.0.0', style: TextStyle(fontSize: 16)),
+          ],
+        ),
       ),
     );
   }
